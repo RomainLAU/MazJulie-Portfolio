16d8364461ccb3c1db8aa99e28474f9756bd63c7
RomainLAU <romain.laurent@epitech.digital>
2023-04-02 18:24:49 +0200

    Update the dropdown menu on mobile

--- dropdown.js
+++ dropdown.js
@@ -1,19 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const burgerButton = document.querySelector('#burger-button');
+  const burgerButtonOpen = document.querySelector('#burger-button-open');
+  const burgerButtonClose = document.querySelector('#burger-button-close');
   const dropdownMenu = document.querySelector('#dropdown-menu');
+  const dropdown = document.querySelector('#dropdown');
   const body = document.querySelector('body');
 
-  burgerButton.style.backgroundImage = "url('./assets/images/burger.png')";
+  burgerButtonOpen.style.backgroundImage = "url('./assets/images/burger.png')";
 
-  burgerButton.addEventListener('click', () => {
-    if (dropdownMenu.style.display == 'flex') {
-      body.classList.remove('no-scroll');
-      burgerButton.style.backgroundImage = "url('./assets/images/burger.png')";
-      dropdownMenu.style.display = 'none';
-    } else {
-      body.classList.add('no-scroll');
-      dropdownMenu.style.display = 'flex';
-      burgerButton.style.backgroundImage = "url('./assets/images/cross.png')";
-    }
+  burgerButtonOpen.addEventListener('click', () => {
+    body.classList.remove('no-scroll');
+    burgerButtonOpen.style.display = 'none';
+    burgerButtonClose.style.display = 'block';
+    dropdownMenu.style.display = 'flex';
+    dropdown.style.display = 'flex';
+  });
+
+  burgerButtonClose.addEventListener('click', () => {
+    body.classList.add('no-scroll');
+    dropdownMenu.style.display = 'none';
+    burgerButtonClose.style.display = 'none';
+    burgerButtonOpen.style.display = 'block';
+    dropdown.style.display = 'none';
   });
 });
